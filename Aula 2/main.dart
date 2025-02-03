33703e6768c895d231117af6874546d32661a624
THIAGO HENRIQUE BENEDITO ALVES <47341687843@educ501.local>
2025-02-03 19:59:21 -0300

    main

---
+++ Aula 2/main.dart
@@ -0,0 +1,23 @@
+// importando a biblioteca do Dart
+import 'dart:io';
+// funçao principal do codigo
+void main(){
+  // declarando a variavel do tipo inteira
+  // chamada idade
+  int idade = 29;
+  print("Idade: $idade");
+
+  // Declarando variavel do tipo double
+  double raio = 10.25;
+  print("Raio: $raio");
+
+  // Declarando variavel do tipo String
+
+  String nome = "Daniel ";
+  print("Nome $nome");
+  print("Ola  $nome! Seja bem vindo ! ");
+
+  // Declarando variavel do tipo booleana
+  bool st= true;
+  print("Estado da variavel st: $st");
+}
